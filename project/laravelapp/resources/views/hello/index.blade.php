f3eed9762311039750451f487967a6f5ba3770bf
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-09-24 12:28:42 +0900

    Install laravel.

---
+++ project/laravelapp/resources/views/hello/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+<p>{{$msg}}</p>
+@if(count($errors) > 0)
+<p>入力に問題があります。再入力してください。</p>
+@endif
+<form action="/hello" method="post">
+<table>
+    @csrf
+    <tr>
+    <th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td>
+    </tr>
+    <tr>
+        <th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td>
+    </tr>
+    <tr>
+        <th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td>
+    </tr>
+    <tr>
+        <th>name: </th><td><input type="submit" value="send"></td>
+    </tr>
+</table>
+</form>
+@endsection
+
+@section('footer')
+    copy-right 2020 hiroaki.
+@endsection
