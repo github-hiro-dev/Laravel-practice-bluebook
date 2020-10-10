4f57214435e6d0916b7fa98ad420061927e421d5
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-10-10 23:18:43 +0900

    Add using global scope search method.

--- project/laravelapp/app/Models/Person.php
+++ project/laravelapp/app/Models/Person.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('age', function(Builder $builder)
+        {
+            $builder->where('age', '>', 20);
+        });
+        
+    }
 
     public function getData()
     {
